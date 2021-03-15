42c97288ffb3307910376b5aed855d2e6135e735
Mani Kumar <mani.s.kumar@aciworldwide.com>
2021-03-15 12:01:28 +0530

    build check

--- src/main/java/com/daylife/school/controller/StudentWelcomeController.java
+++ src/main/java/com/daylife/school/controller/StudentWelcomeController.java
@@ -14,6 +14,7 @@ public class StudentWelcomeController {
   public User welcomeStudent(@RequestBody User user) {
     user.setName(user.getName());
     user.setAge(user.getAge());
+    user.setBranch("CSE");
     return user;
   }
 
--- src/main/java/com/daylife/school/model/User.java
+++ src/main/java/com/daylife/school/model/User.java
@@ -9,5 +9,6 @@ public class User {
   
   private String name;
   private int age;
+  private String branch;
 
 }
